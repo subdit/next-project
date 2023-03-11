34f65c092f2c7bce00b5196bb5e6344362d9c803
Kasidit Bualoeng <subdit@gmail.com>
2023-03-11 17:02:13 -0500

    add Head and Title for the meta search

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a4b18251dbe0b9b8"; }
+/******/ 		__webpack_require__.h = function() { return "db425027931440db"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ function Homepage() {
     <>
       <Head>
         <title>My Blog</title>
-        <meta name='decriptiton' value='this is my blog' />
+        <meta name='decriptiton' value='This is my blog' />
       </Head>
       <main>
         <h2>My blog Home Page build by Next.js.!</h2>
